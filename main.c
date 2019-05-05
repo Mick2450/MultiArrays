7896575e4d2fe95650b894a8ab3da9161836f2ec
Michael Maurice <michaelmaurice92@gmail.com>
2019-05-05 03:18:00 +0000

    Prints values of 3x3 array and calculates mean value of array

--- main.c
+++ main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main(){
+    float myArray[3][3] = { { 0.1, 0.2, 0.3 }, { 1.1, 1.2, 1.3 }, { 2.1, 2.2, 2.3 } };
+    float sum = 0, value, mean;
+    int row, col;
+    
+    for(row = 0; row < 3; row++){
+        for(col = 0; col < 3; col++){
+            
+        printf("%f ", myArray[row][col]);
+        }
+        printf("\n");
+    }
+    
+    for(row = 0; row < 3; row++){
+        for(col = 0; col < 3; col++){
+            
+        value = myArray[row][col];
+        sum = sum + value; 
+        
+        }
+    }
+    mean = sum/9;
+    printf("\nmean of array is %f\n", mean);
+    
+    return 0;
+}
