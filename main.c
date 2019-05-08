f8bebbc541037adef86cb863d50904a56862362d
Michael Maurice <michaelmaurice92@gmail.com>
2019-05-08 03:43:51 +0000

    re-did task 4 from scratch

--- main.c
+++ main.c
@@ -2,27 +2,16 @@
 
 int main(){
     float myArray[3][3] = { { 0.1, 0.2, 0.3 }, { 1.1, 1.2, 1.3 }, { 2.1, 2.2, 2.3 } };
-    float sum = 0, value, mean;
     int row, col;
+    float sum = 0, mean;
     
     for(row = 0; row < 3; row++){
         for(col = 0; col < 3; col++){
-            
-        printf("%f ", myArray[row][col]);
-        }
-        printf("\n");
-    }
-    
-    for(row = 0; row < 3; row++){
-        for(col = 0; col < 3; col++){
-            
-        value = myArray[row][col];
-        sum = sum + value; 
-        
+          sum += myArray[row][col];  
         }
     }
-    mean = sum/9;
-    printf("\nmean of array is %f\n", mean);
-    
+    mean = sum/9; 
+    printf("\nmean of array values is %f\n", mean);
+   
     return 0;
 }
